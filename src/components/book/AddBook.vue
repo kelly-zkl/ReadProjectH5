32ae198221394b1b566ff158eeb0d6fb33556d83
kelly <1457242108@qq.com>
2018-08-18 16:34:56 +0800

    修改标签页面

--- src/components/book/AddBook.vue
+++ src/components/book/AddBook.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
     <section>
-
+      <el-form :model="book" align="left" label-position="left" label-width="80px">
+        <el-row style="background: #F2F2F2">
+          <el-col :span="8" align="left">
+            <div style="font-size:14px;padding:10px 20px">资源信息</div>
+          </el-col>
+        </el-row>
+        <div class="add-appdiv" style="border-top: none;border-radius: 0 0 4px 4px">
+          <el-form-item label="ISBN">
+            <el-input v-model="book.isbn" :maxlength=30 placeholder="书名/系列名"></el-input>
+          </el-form-item>
+        </div>
+        <el-row style="background: #F2F2F2">
+          <el-col :span="8" align="left">
+            <div style="font-size:14px;padding:10px 20px">添加资源</div>
+          </el-col>
+        </el-row>
+        <div class="add-appdiv" style="border-top: none;border-radius: 0 0 4px 4px;padding-top: 0">
+          <el-row>
+            <el-col :span="8" align="left">
+              <el-button type="text">删除</el-button>
+            </el-col>
+            <el-col :span="8" :offset="8" align="right">
+              <el-button type="text">增加一页</el-button>
+            </el-col>
+          </el-row>
+        </div>
+      </el-form>
+      <div align="center">
+        <el-button type="primary" @click="">确认</el-button>
+      </div>
     </section>
   </div>
 </template>
 <script>
   export default {
     data() {
-      return {}
+      return {
+        book: {}
+      }
     },
     methods: {},
     mounted() {
--- src/components/book/TagList.vue
+++ src/components/book/TagList.vue
@@ -1,51 +1,77 @@
 <template>
   <div>
     <section>
-      <el-form :inline="true" :model="query" align="left" style="margin-bottom: 10px">
-        <el-row>
-          <el-col :span="16" align="left">
-            <el-form-item style="margin-bottom: 10px">
-              <el-input v-model="query.name" size="medium" :maxlength=30 placeholder="标签名称"></el-input>
-            </el-form-item>
-            <el-form-item style="margin-bottom: 10px">
-              <el-button type="primary" size="medium" @click="getData()">搜索</el-button>
-            </el-form-item>
-            <el-form-item style="margin-bottom: 10px">
-              <el-button type="text" @click="clearData()">重置</el-button>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8" align="right">
-            <el-form-item style="margin-bottom: 10px">
-              <el-button type="primary" size="medium" @click="runAddTag = true">添加标签</el-button>
-            </el-form-item>
-          </el-col>
-        </el-row>
-      </el-form>
-
-      <el-table :data="tagList" v-loading="listLoading" class="center-block" stripe>
-        <el-table-column type="selection" width="45" align="left"></el-table-column>
-        <el-table-column align="center" type="index" label="序号" width="65"></el-table-column>
-        <el-table-column align="left" prop="deviceName" label="标签" :formatter="formatValue"></el-table-column>
-        <el-table-column align="left" prop="ISBN" label="状态" :formatter="formatValue"></el-table-column>
-        <el-table-column align="left" label="操作">
-          <template slot-scope="scope">
-            <el-button type="text" @click="">启用</el-button>
-            <el-button type="text" @click="">修改</el-button>
-            <el-button type="text" @click="">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <div class="block" style="margin-top: 20px" align="right">
-        <el-pagination
-          @size-change="handleSizeChange" @current-change="pageChange" :current-page="query.page" background
-          :page-sizes="[10, 15, 20, 30]" :page-size="query.size" layout="total, sizes, prev, pager, next, jumper"
-          :total="count"></el-pagination>
-      </div>
+      <el-row :gutter="20">
+        <el-col :span="6" style="border:1px #f2f2f2 solid;padding: 0">
+          <div>
+            <el-button type="primary" size="mini" style="width: 100%;border-radius: 0"
+                       @click="runAddClass=true;">添加分类
+            </el-button>
+          </div>
+          <div v-for="(item,idx) in [1,2,3,4,5,6]" v-bind:class="indx==idx?'tag select':'tag'"
+               @click="handleSelectItem(idx)">分类A{{item}}
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <el-form :inline="true" :model="query" align="left" style="margin-bottom: 10px">
+            <el-row>
+              <el-col :span="16" align="left">
+                <el-form-item style="margin-bottom: 10px">
+                  <el-button type="text">停用分类</el-button>
+                </el-form-item>
+                <el-form-item style="margin-bottom: 10px">
+                  <el-button type="text">修改分类</el-button>
+                </el-form-item>
+                <el-form-item style="margin-bottom: 10px">
+                  <el-input v-model="query.name" size="medium" :maxlength=30 placeholder="标签名称"></el-input>
+                </el-form-item>
+                <el-form-item style="margin-bottom: 10px">
+                  <el-button type="primary" size="medium" @click="getData()">搜索</el-button>
+                </el-form-item>
+                <el-form-item style="margin-bottom: 10px">
+                  <el-button type="text" @click="clearData()">重置</el-button>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8" align="right">
+                <el-form-item style="margin-bottom: 10px">
+                  <el-button type="primary" size="medium" @click="runAddTag = true">添加标签</el-button>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+          <el-table :data="tagList" v-loading="listLoading" class="center-block" stripe>
+            <el-table-column type="selection" width="45" align="left"></el-table-column>
+            <el-table-column align="center" type="index" label="序号" width="65"></el-table-column>
+            <el-table-column align="left" prop="deviceName" label="标签" :formatter="formatValue"></el-table-column>
+            <el-table-column align="left" prop="ISBN" label="状态" :formatter="formatValue"></el-table-column>
+            <el-table-column align="left" label="操作">
+              <template slot-scope="scope">
+                <el-button type="text" @click="">启用</el-button>
+                <el-button type="text" @click="">修改</el-button>
+                <el-button type="text" @click="">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="block" style="margin-top: 20px" align="right">
+            <el-pagination
+              @size-change="handleSizeChange" @current-change="pageChange" :current-page="query.page" background
+              :page-sizes="[10, 15, 20, 30]" :page-size="query.size" layout="total, sizes, prev, pager, next, jumper"
+              :total="count"></el-pagination>
+          </div>
+        </el-col>
+      </el-row>
+      <!--添加分类-->
+      <el-dialog title="添加分类" :visible.sync="runAddClass" center width="500px">
+        <el-input v-model="className" size="medium" :maxlength=30 placeholder="分类名称"></el-input>
+        <div slot="footer" class="dialog-footer" align="center">
+          <el-button type="primary" @click="addClass()">确认添加</el-button>
+        </div>
+      </el-dialog>
       <!--添加标签-->
       <el-dialog title="添加标签" :visible.sync="runAddTag" center width="500px">
         <el-input v-model="tagName" size="medium" :maxlength=30 placeholder="标签名称"></el-input>
         <div slot="footer" class="dialog-footer" align="center">
-          <el-button type="primary" @click="runAddTag = false">确认</el-button>
+          <el-button type="primary" @click="addTag()">确认添加</el-button>
         </div>
       </el-dialog>
     </section>
@@ -57,13 +83,27 @@
       return {
         listLoading: false,
         runAddTag: false,
+        runAddClass: false,
+        className: '',
         query: {page: 1, size: 10},
         tagList: [],
         count: 0,
-        tagName: ''
+        tagName: '',
+        indx: 0
       }
     },
     methods: {
+      //添加标签
+      addTag() {
+        this.runAddTag = false;
+      },
+      //添加分类
+      addClass() {
+        this.runAddClass = false;
+      },
+      handleSelectItem(val) {
+        this.indx = val;
+      },
       clearData() {
         this.query = {page: 1, size: 10};
         this.getData();
@@ -92,3 +132,17 @@
     }
   }
 </script>
+<style scoped>
+  .tag {
+    height: 56px;
+    line-height: 56px;
+    background: #E4E4E4;
+    cursor: pointer;
+    border-top: 1px #fff solid;
+  }
+
+  .tag.select, .tag:hover, .tag:active {
+    background: #fff;
+    border-left: 5px #6699FF solid;
+  }
+</style>
